822578efeaac37261d9da6088bf5fcbf8beb6db7
Piotr Lezanski <lezanskipiotr@gmail.com>
2023-04-06 18:20:55 +0200

    comm

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,10 +1,80 @@
 #include <iostream>
+#include <vector>
 #include "Matrix.h"
 
-Matrix::Matrix() {
-    std::cout<< "HUYJ" <<std::endl;
+// constuctors & destructors
+Matrix::Matrix() {}
+
+Matrix::Matrix(int r, int c) {
+    rowNum = r;
+    colNum = c;
+    mat = new double[rowNum*colNum];
+    memset(mat, 0, rowNum*colNum*sizeof(double));
 }
 
-int Matrix::add(int a, int b) {
-    return a+b;
+Matrix::Matrix(int r, int c, const std::vector<double> arr) {
+    if( arr.size() == rowNum * colNum ) {
+        Matrix(r, c);
+        for(int i=0; i<rowNum*colNum; ++i)
+            mat[i] = arr[i];   
+    }
+    else {
+        std::cerr<< "Wrong size of input array" <<std::endl;
+        std::abort();
+    }
 }
+
+Matrix::Matrix(const Matrix& copy) {
+    Matrix(copy.rowNum, copy.colNum);
+    for(int i=0; i<rowNum*colNum; ++i) 
+        mat[i] = copy.mat[i];
+}
+
+Matrix::~Matrix() {
+    if( mat != nullptr ) 
+        delete [] mat;
+}
+
+// core functions
+void Matrix::print()
+{
+    for(int i=0; i<rowNum; ++i) {
+        for(int j=0; j<colNum; ++j) {
+            std::cout<< mat[i*rowNum+j] <<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+// operations
+
+
+// overloaded operators
+
+double Matrix::operator()(size_t r, size_t c) const
+{
+    if( r < rowNum && c < colNum ) {
+        return mat[r*rowNum + colNum];
+    }   
+    else {
+        std::cerr<< "Wrong indexes" <<std::endl;
+        std::abort();
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, const Matrix& x) {
+    for(int i=0; i<x.rowNum; ++i) {
+        for(int j=0; j<x.colNum; ++j) {
+            out<< x(i, j) << " ";
+        }
+        out<< std::endl;
+    }
+    return out;
+}
+
+// std::istream& operator>>(std::istream& in, const Matrix& x) {
+//     for(auto &a : x) {
+//         in >> a;
+//     }
+//     return in;
+// }
--- Matrix.h
+++ Matrix.h
@@ -1,11 +1,35 @@
 // #include <iostream>
 #ifndef MATRIX_H
 #define MATRIX_H
+#include <vector>
 
 class Matrix {
+private:
+    size_t rowNum;
+    size_t colNum;
+    double* mat;
 public:
+    // constuctors & destructors
     Matrix();
-    int add(int, int);
+    Matrix(int, int); // initialize nxm empty matrix (filled with 0)
+    Matrix(int, int, const std::vector<double>); // ilitialize nxm matrix with given array
+    Matrix(const Matrix&); // copy constructor
+    ~Matrix(); 
+
+    // core functions
+    int getRowNum() {return rowNum;};
+    int getColNum() {return colNum;};
+    void print();
+
+    // operations
+    void transpose(Matrix&);
+    void rotate(Matrix&);
+    double determinant(Matrix&);
+
+    // overloaded operators
+    double operator()(size_t, size_t) const;
+    friend std::ostream& operator<<(std::ostream&, const Matrix&);
+    friend std::istream& operator>>(std::istream&, const Matrix&);
 };
 
 #endif
